dc9f2f7b1e1e76055f70b24440997ea9921221ec
VittorioIusi <vittorioiusi47@gmail.com>
2024-07-06 18:54:05 +0200

    inizio

---
+++ gestoreCampi/src/main/java/com/example/gestoreCampi/GestoreCampiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestoreCampi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestoreCampiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestoreCampiApplication.class, args);
+	}
+
+}
---
+++ gestoreCampi/src/test/java/com/example/gestoreCampi/GestoreCampiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestoreCampi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestoreCampiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
